8edc7f7e67422409fd432cb2409f656095452e2e
mario <maogaz@uc.cl>
2020-04-29 02:48:39 -0400

    primer commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+
+
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.use(require('./routes/index'));
+app.use('/api/hamburguesas',require('./routes/hamburgers'));
+
+app.use('/api/ingredientes',require('./routes/ingredients'));
+
+
+
+
+
+app.listen(app.get('port'),() => { 
+    console.log(`Server on port ${app.get("port")}`);
+});
---
+++ src/routes/hamburgers.js
@@ -0,0 +1,76 @@
+const { Router } = require('express');
+const router = Router();
+
+const _ = require('underscore');
+
+const hamburgers = require('../sample.json');
+
+
+router.get('/', (req, res) => {
+    res.status(200).json(hamburgers);
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    _.each(hamburgers, (hamburger,i) => {
+        if (hamburger.id == id) {
+            res.status(200).send(hamburger);
+        } 
+    });
+    res.status(404).send('Hamburguesa inexistente');
+
+});
+
+
+router.post('/', (req, res) => {
+    const { nombre, precio, descripcion, imagen}= req.body;
+    const ingredientes = [{}];
+    if (nombre && precio && descripcion && imagen){
+        const id = hamburgers.length + 1;
+        const newHamburger = {...req.body, id, ingredientes};
+        hamburgers.push(newHamburger);
+        res.json(hamburgers);
+
+    } else {
+        res.status(400).send('Input invalido');
+    }
+});
+
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    const { nombre, precio, descripcion, imagen} = req.body;
+    if (nombre && precio && descripcion && imagen) {
+        _.each(hamburgers, (hamburger, i) => {
+            if (hamburger.id == id) {
+                hamburger.nombre = nombre;
+                hamburger.precio = precio;
+                hamburger.descripcion = descripcion;
+                hamburger.imagen = imagen;
+                res.status(200).send('Operacion exitosa');
+            }
+        });
+        //res.status(200).send('Operacion exitosa');
+
+    } else {
+        res.status(400).send('Paramentros invalidos');
+    }
+    res.status(404).send('Hamburguesa inexistente');
+});
+
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    _.each(hamburgers, (hamburger,i) => {
+        if (hamburger.id == id) {
+            hamburgers.splice(i,1);
+            res.status(200).send('Hamburguesa eliminada');
+        }
+        else {
+            res.status(404).send('Hamburguesa inexistente');
+        }
+    });
+    //res.status(200).send('Hamburguesa eliminada');
+});
+
+
+module.exports = router;
