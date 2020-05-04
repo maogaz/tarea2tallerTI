e119de22f9867c07ac7f09484714ff853afef99d
mario <maogaz@uc.cl>
2020-05-04 19:08:48 -0400

    actualizar ingredientes

--- src/routes/ingredients.js
+++ src/routes/ingredients.js
@@ -15,19 +15,18 @@ router.get('/:id', (req, res) => {
     _.each(hamburgers, (hamburger,i) => {
         if (hamburger.id == id) {
             res.status(200).send(hamburger);
-        } 
+        }
     });
-    res.status(404).send('Hamburguesa inexistente');
+    res.status(404).send('Ingrediente inexistente');
 
 });
 
 
 router.post('/', (req, res) => {
-    const { nombre, precio, descripcion, imagen}= req.body;
-    const ingredientes = [{}];
-    if (nombre && precio && descripcion && imagen){
+    const { nombre, descripcion}= req.body;
+    if (nombre && descripcion){
         const id = hamburgers.length + 1;
-        const newHamburger = {...req.body, id, ingredientes};
+        const newHamburger = {...req.body, id};
         hamburgers.push(newHamburger);
         res.json(hamburgers);
 
@@ -38,14 +37,12 @@ router.post('/', (req, res) => {
 
 router.patch('/:id', (req, res) => {
     const { id } = req.params;
-    const { nombre, precio, descripcion, imagen} = req.body;
-    if (nombre && precio && descripcion && imagen) {
+    const { nombre, descripcion} = req.body;
+    if (nombre && descripcion) {
         _.each(hamburgers, (hamburger, i) => {
             if (hamburger.id == id) {
                 hamburger.nombre = nombre;
-                hamburger.precio = precio;
                 hamburger.descripcion = descripcion;
-                hamburger.imagen = imagen;
                 res.status(200).send('Operacion exitosa');
             }
         });
@@ -54,7 +51,7 @@ router.patch('/:id', (req, res) => {
     } else {
         res.status(400).send('Paramentros invalidos');
     }
-    res.status(404).send('Hamburguesa inexistente');
+    res.status(404).send('Ingrediente inexistente');
 });
 
 
@@ -63,10 +60,10 @@ router.delete('/:id', (req, res) => {
     _.each(hamburgers, (hamburger,i) => {
         if (hamburger.id == id) {
             hamburgers.splice(i,1);
-            res.status(200).send('Hamburguesa eliminada');
+            res.status(200).send('Ingrediente eliminado');
         }
         else {
-            res.status(404).send('Hamburguesa inexistente');
+            res.status(404).send('Ingrediente inexistente');
         }
     });
     //res.status(200).send('Hamburguesa eliminada');
